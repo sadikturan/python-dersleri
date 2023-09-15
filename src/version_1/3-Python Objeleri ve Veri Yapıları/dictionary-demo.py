768713e3e0f6bb672c91c7d315d7a08fe5a5e79c
Ramazan <139284128+RamazzanErdogan@users.noreply.github.com>
2023-09-16 02:48:26 +0300

    dictionary and while
    sözlük(dictionary) ve while döngüsü uygulaması

--- src/version_1/3-Python Objeleri ve Veri Yapıları/dictionary-demo.py
+++ src/version_1/3-Python Objeleri ve Veri Yapıları/dictionary-demo.py
@@ -1,79 +1,17 @@
-'''
-    ogrenciler = {
-        '120': {
-            'ad': 'Ali',
-            'soyad': 'Yılmaz',
-            'telefon': '532 000 00 01'
-        },
-        '125': {
-            'ad': 'Can',
-            'soyad': 'Korkmaz',
-            'telefon': '532 000 00 02'
-        },
-        '128': {
-            'ad': 'Volkan',
-            'soyad': 'Yükselen',
-            'telefon': '532 000 00 03'
-        },
-    }
 
-    1- Bilgileri verilen öğrencileri kullanıcıdan aldığınız bilgilerle
+    # 1- Bilgileri verilen öğrencileri kullanıcıdan aldığınız bilgilerle
        dictionary içinde saklayınız.
-
-    2- Öğrenci numarasını kullanıcıdan alıp ilgili öğrenci bilgisini gösterin.
-'''
-
-ogrenciler = {}
-
-number = input("öğrenci no: ")
-name = input("öğrenci adı: ")
-surname = input("öğrenci soyad: ")
-phone = input("öğrenci telefon: ")
-
-# ogrenciler[number] = {
-#     'ad': name,
-#     'soyad': surname,
-#     'telefon': phone
-# }
-
-ogrenciler.update({
-    number: {
-        'ad': name,
-        'soyad': surname,
-        'telefon':phone 
-    }
-})
-
-number = input("öğrenci no: ")
-name = input("öğrenci adı: ")
-surname = input("öğrenci soyad: ")
-phone = input("öğrenci telefon: ")
-
-ogrenciler.update({
-    number: {
-        'ad': name,
-        'soyad': surname,
-        'telefon':phone 
-    }
-})
-
-number = input("öğrenci no: ")
-name = input("öğrenci adı: ")
-surname = input("öğrenci soyad: ")
-phone = input("öğrenci telefon: ")
-
-ogrenciler.update({
-    number: {
-        'ad': name,
-        'soyad': surname,
-        'telefon':phone 
-    }
-})
-
-print('*'*50)
-
-ogrNo = input('öğrenci no: ')
-ogrenci = ogrenciler[ogrNo]
-print(ogrenci)
-
-print(f"Aradığınız {ogrNo} nolu öğrencinin adı: {ogrenci['ad']} soyadı: {ogrenci['soyad']} ve telefonu ise {ogrenci['telefon']}")
+    # 2- Öğrenci numarasını kullanıcıdan alıp ilgili öğrenci bilgisini gösterin.
+ögrenciler={}
+x=int(input("Kaç öğrenci bilgisini Dictionary sözlük içersine saklamak istiyorsunuz==\n"))
+i=0
+while i<x:
+        numara=input("Numaranızı giriniz=")
+        ad=input("Adınızı giriniz=")
+        soyad=input("Soyadınız girniz=")
+        tel=input("TEL=")
+        ögrenciler.update({numara:{"AD":ad,"Soyad":soyad,"TEL":tel}})
+        i+=1
+# Öğrenci numarasını kullanıcıdan alıp öğrenci bilgilerini gösterin
+ogrno=input("Aramak istediğiniz öğrenci numarası giriniz= ")
+print(ögrenciler[ogrno])
