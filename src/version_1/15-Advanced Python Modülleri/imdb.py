d12a641d6adb27c6fe62daeb201a53c13c839ff6
“sadikturan” <sadikturan@gmail.com>
2023-11-09 14:13:04 +0300

    imdb scraping update

--- src/version_1/15-Advanced Python Modülleri/imdb.py
+++ src/version_1/15-Advanced Python Modülleri/imdb.py
@@ -1,18 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.imdb.com/chart/top?ref_=nv_mv_250"
+url = "https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250"
 
-html = requests.get(url).content
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0"
+}
+
+html = requests.get(url, headers=headers).content
 soup = BeautifulSoup(html, "html.parser")
 
-list = soup.find("tbody", {"class":"lister-list"}).find_all("tr",limit=50)
-count = 1
+liste = soup.find("ul", {"class":"ipc-metadata-list"}).find_all("li", limit=100)
 
-for tr in list:
-    title = tr.find("td",{"class":"titleColumn"}).find("a").text
-    year = tr.find("td",{"class":"titleColumn"}).find("span").text.strip("()")
-    rating = tr.find("td", {"class":"ratingColumn imdbRating"}).find("strong").text
+for item in liste:
+    filmadi = item.find("h3", {"class":"ipc-title__text"}).text
+    puan = item.find("span", {"class":"ipc-rating-star"}).text
 
-    print(f"{count}- film: {title.ljust(50)} yıl: {year} değerlendirme: {rating}")
-    count+=1
+    print(filmadi, puan)
